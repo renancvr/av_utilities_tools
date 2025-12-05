9836ae4aab9cd650a58d6b417643f4a026ac6dce
renancvr <renancvr20@gmail.com>
2025-12-05 15:26:15 -0300

    Update configure.py

--- av_access/auto_ip_setup/configure.py
+++ av_access/auto_ip_setup/configure.py
@@ -3,23 +3,43 @@ import sys
 import os
 import time
 
-file_path = sys.argv[1]
+def config_manual():
 
-df = pd.read_excel(file_path)
+    total_devices = int(input("Total of encoders/decoders: "))
 
-ips = df.values.tolist()
+    for i in range(total_devices):
+        print(f"Device {i+1}")
+        old_ip_addr = input("Old IP Address: ")
+        new_ip_addr = input("New IP Address: ")
+
+        os.system(f"python telnetclient.py {old_ip_addr} {new_ip_addr}")
+
+def config_via_sheet():
+
+    file_path = sys.argv[1]
+
+    df = pd.read_excel(file_path)
+
+    ips = df.values.tolist()
 
 
-for ip in ips:
+    for ip in ips:
 
-    old_ip_addr = ip[0]
-    new_ip_addr = ip[1]
+        old_ip_addr = ip[0]
+        new_ip_addr = ip[1]
 
-    print(f"Changing ip {old_ip_addr} to {new_ip_addr}")
-    for i in range(50):
-        print('#', end='',flush=True)
-        time.sleep(0.03)
-    print('\n')
+        print(f"Changing ip {old_ip_addr} to {new_ip_addr}")
+        for i in range(50):
+            print('#', end='',flush=True)
+            time.sleep(0.03)
+        print('\n')
 
 
-    os.system(f"python telnetclient.py {old_ip_addr} {new_ip_addr}")
+        os.system(f"python telnetclient.py {old_ip_addr} {new_ip_addr}")
+
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        config_via_sheet()
+    else:
+        config_manual()
